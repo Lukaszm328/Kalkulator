4101622ca8cdfe2aef22b97ff84e1f8b93d6a430
Lukasz <Lukaszmpakietm3@gmail.com>
2018-06-30 20:30:55 +0200

    Pierwsza wersja
    Dodałem pliki

---
+++ Syntezator dla niewidomych/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Syntezator_dla_niewidomych
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(28, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aby korzystać z kalkulatora należy używać liczb z NumPad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(28, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Opis przycisków :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ESC - zamyka program";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Prawy Ctrl - Pomoc";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 73);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operacje na kalkulatorze";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(247, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "brak";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(238, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Operacja";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(303, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Wynik";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(17, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Location = new System.Drawing.Point(26, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 45);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opcje :";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label15.Location = new System.Drawing.Point(247, 21);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 13);
+            this.label15.TabIndex = 4;
+            this.label15.Text = "100 %";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(159, 22);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(89, 13);
+            this.label14.TabIndex = 3;
+            this.label14.Text = "Głośność głosu :";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label13.Location = new System.Drawing.Point(139, 22);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(18, 13);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "-3";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(64, 21);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(76, 13);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "Tempo głosu :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label9.Location = new System.Drawing.Point(9, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 15);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "SPEAK";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(148, 196);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "<- -> Tempo głosu";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(148, 220);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(75, 13);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "^ v - głośnośc";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(295, 206);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "NumLock";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 254);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalkulator dla niewidomych wersja 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Syntezator dla niewidomych/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Syntezator_dla_niewidomych
+{
+    public partial class Form1 : Form
+    {
+
+        // Initialize a new instance of the SpeechSynthesizer.
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        double number1, number2, result;
+        bool plus = false, minus = false, dziel = false, mnoz = false;
+        public Form1()
+        {
+            InitializeComponent();
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+            // Default speed voice 
+            synth.Rate = -3;
+            // Default volume voice
+            synth.Volume = 100;
+        }
+
+        public void repeat()
+        {
+            if (textBox1.Text == "")
+            {
+                speak("Brak danych.");
+            }
+            else
+            {
+                speak(textBox1.Text);
+            } 
+        }
+
+        public void speak_calc(string a)
+        {
+            speak(a);
+            textBox1.Text = textBox1.Text + a;
+        }
+
+        public void speak(string b)
+        {
+            label9.BackColor = Color.Red;
+            synth.Speak(b);
+            label9.BackColor = Color.Empty;
+        }
+
+        public void speed_voice_up()
+        {
+            if ( synth.Rate == 10 )
+            {
+                speak("Tempo najszybsze.");
+            }
+            else
+            {
+                synth.Rate = synth.Rate + 1;
+                speak("Prędkość mówienia " + synth.Rate);
+                string tempo = synth.Rate.ToString();
+                label13.Text = tempo;
+            }
+        }
+
+        public void speed_voice_down()
+        {
+            if (synth.Rate == -10)
+            {
+                speak("Tempo najniższe.");
+            }
+            else
+            {
+                synth.Rate = synth.Rate - 1;
+                speak("Prędkość mówienia " + synth.Rate);
+                string tempo = synth.Rate.ToString();
+                label13.Text = tempo;
+            }
+        }
+
+        public void volume_voice_up()
+        {
+            if (synth.Volume == 100)
+            {
+                speak("Głośność maksymalna.");
+            }
+            else
+            {
+                synth.Volume = synth.Volume + 10;
+                speak("Podgłaszam głos");
+                int g = synth.Volume;
+                label15.Text = g.ToString();
+            }
+        }
+        public void volume_voice_down()
+        {
+            if (synth.Volume == 10)
+            {
+                speak("Głośność minimalna.");
+            }
+            else
+            {
+                synth.Volume = synth.Volume - 10;
+                speak("ściszam głos");
+                int g = synth.Volume;
+                label15.Text = g.ToString();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.NumPad1)
+            {
+                speak_calc("1");
+            }
+
+            if (e.KeyCode == Keys.NumPad2)
+            {
+                speak_calc("2");
+            }
+            if (e.KeyCode == Keys.NumPad3)
+            {
+                speak_calc("3");
+            }
+            if (e.KeyCode == Keys.NumPad4)
+            {
+                speak_calc("4");
+            }
+            if (e.KeyCode == Keys.NumPad5)
+            {
+                speak_calc("5");
+            }
+            if (e.KeyCode == Keys.NumPad6)
+            {
+                speak_calc("6");
+            }
+            if (e.KeyCode == Keys.NumPad7)
+            {
+                speak_calc("7");
+            }
+            if (e.KeyCode == Keys.NumPad8)
+            {
+                speak_calc("8");
+            }
+            if (e.KeyCode == Keys.NumPad9)
+            {
+                speak_calc("9");
+            }
+            if (e.KeyCode == Keys.NumPad0)
+            {
+                speak_calc("0");
+            }
+
+            if (e.KeyCode == Keys.ShiftKey)
+            {
+                repeat();
+            }
+
+            if (e.KeyCode == Keys.Back)
+            {
+                if (textBox1.Text == "")
+                {
+                    speak("Brak liczby do usunięcia.");
+                }
+                else
+                {
+                    // Deleted last number from textBox1
+                    speak("Usówam liczbę");
+                    textBox1.Text = textBox1.Text.Remove((textBox1.Text.Length - 1), 1);
+                }
+
+            }
+
+            if (e.KeyCode == Keys.Add)
+            {
+                if (textBox1.Text == "")
+                {
+                    speak("Najpierew wprowadź dane do obliczeń z klawiatury NumPad");
+                }
+                else
+                {
+                    speak("Dodać");
+                    number1 = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    plus = true;
+                    label8.Text = "+";
+                }
+            }
+
+            if (e.KeyCode == Keys.Subtract)
+            {
+                if (textBox1.Text == "")
+                {
+                    speak("Najpierew wprowadź dane do obliczeń z klawiatury NumPad");
+                }
+                else
+                {
+                    speak("odjąć");
+                    number1 = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    minus = true;
+                    label8.Text = "-";
+                }
+            }
+
+            if (e.KeyCode == Keys.Multiply)
+            {
+                if (textBox1.Text == "")
+                {
+                    speak("Najpierew wprowadź dane do obliczeń z klawiatury NumPad");
+                }
+                else
+                {
+                    speak("pomnożyć");
+                    number1 = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    mnoz = true;
+                    label8.Text = "*";
+                }
+            }
+
+            if (e.KeyCode == Keys.Divide)
+            {
+                if (textBox1.Text == "")
+                {
+                    speak("Najpierew wprowadź dane do obliczeń z klawiatury NumPad");
+                }
+                else
+                {
+                    speak("podzielić");
+                    number1 = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    dziel = true;
+                    label8.Text = "/";
+                }
+            }
+
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                speak("Pomoc");
+            }
+
+            if (e.KeyCode == Keys.Decimal)
+            {
+                speak("Przecinek");
+                textBox1.Text = textBox1.Text + ",";
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                speak("Zamykam program");
+                Close();
+            }
+            if (e.KeyCode == Keys.Home)
+            {
+                speak("Pomoc");
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                volume_voice_up();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                volume_voice_down();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                speed_voice_down();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                speed_voice_up();
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (plus == true)
+                {
+                    if (textBox1.Text == "")
+                    {
+                        speak("Wprowadź najpierw liczbę do dodania");
+                    }
+                    else
+                    {
+                        number2 = double.Parse(textBox1.Text);
+                        result = number1 + number2;
+
+                        plus = false;
+                    }
+                }
+
+                if (minus == true)
+                {
+                    if (textBox1.Text == "")
+                    {
+                        speak("Wprowadź najpierw liczbę do odjęcia");
+                    }
+                    else
+                    {
+                        number2 =  double.Parse(textBox1.Text);
+                        result = number1 - number2;
+
+                        minus = false;
+                    }
+                }
+                if (dziel == true)
+                {
+                        if (textBox1.Text == "")
+                        {
+                            speak("Wprowadź najpierw liczbę do dzielenia");
+                        }
+                        else if (number2 == 0)
+                    {
+                        speak("Pamiętaj cholero nie dziel przez 0 !");
+                    }
+                        else
+                        {
+                            number2 = double.Parse(textBox1.Text);
+                            result = number1 / number2;
+
+                            dziel = false;
+                        }
+                }
+
+                if (mnoz == true)
+                {
+                    if (textBox1.Text == "")
+                    {
+                        speak("Wprowadź najpierw liczbę do pomnozenia");
+                    }
+                    else
+                    {
+                        number2 = double.Parse(textBox1.Text);
+                        result = number1 * number2;
+
+                        mnoz = false;
+                    }
+                }
+
+                string results = result.ToString();
+                speak("Wynik działania" + number1 + label8.Text + number2 + " to " + results);
+                label6.Text = results;
+                textBox1.Text = "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speak("Witaj w programie kalkulator, aby odsłuchać pomoc naciśnij prawy kontrol");
+        }
+        }
+    }
+
